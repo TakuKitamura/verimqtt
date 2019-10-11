466085fafcde525da4a68c2039891987e339b34b
TakuKitamura <tikutaku0128@icloud.com>
2019-10-11 16:26:59 +0900

    change return value in parse-func

--- src/callMain.c
+++ src/callMain.c
@@ -3,25 +3,33 @@
 
 #include "Main.h"
 
+// typedef struct Data
+// {
+//   uint8_t r;
+//   uint8_t g;
+//   uint8_t b;
+// }
+// data_s;
+
 int main(int argc, char *argv[]) {
     FILE *fp;
     char *fname = "publishMessagePacket.bin";
     uint8_t request[10000];
     uint32_t  i;
     uint32_t size;
+    // data_struct data;
 
-    fp = fopen( fname, "rb" );
+    fp = fopen(fname, "rb");
     if( fp == NULL ){
-    printf( "%sファイルが開けません", fname );
-    return -1;
+        printf("%sファイルが開けません", fname);
+        return 1;
     }
 
     size = fread( request, sizeof( unsigned char ), 10000, fp );
     fclose(fp);
-    // for(i=0; i<size; i++ ){
-    //     printf("%02x\n", request[i]);
-    // }
     
-    parse(request, size);
+    data_struct data = parse(request, size);
+
+    printf("r=%uy\n", data.r);
     return 0;
 }
