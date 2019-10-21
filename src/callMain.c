bafedcbfccbf1c3911055514df760f6b0ed369ed
TakuKitamura <tikutaku0128@icloud.com>
2019-10-21 17:57:49 +0900

    improve error handler

--- src/callMain.c
+++ src/callMain.c
@@ -26,12 +26,13 @@ int main(int argc, char *argv[]) {
 
     // parse関数が定理証明済み
     struct_fixed_header data = parse(request, tcp_payload);
-    free(request); 
+    free(request);
 
     printf("message_name=%s\n", data.message_name);
-    printf("message_flag=%04x\n", data.message_type);
-    printf("dup_flag=%01x\n", data.flags.dup_flag);
-    printf("qos_flag=%02x\n", data.flags.qos_flag);
+    printf("message_type=%04x\n", data.message_type);
+    printf("flag=%04x\n", data.flags.flag);
+    printf("dup_flag=%04x\n", data.flags.dup_flag);
+    printf("qos_flag=%04x\n", data.flags.qos_flag);
     printf("retain_flag=%01x\n", data.flags.retain_flag);
     printf("remaining_length=%u\n", data.remaining_length);
     printf("error_message=\"%s\"\n", data.error_message);
